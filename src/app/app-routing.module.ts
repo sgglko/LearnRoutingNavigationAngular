c16786cc5f3061626c0a39f12ef8e8e236296463
Praktikant <Praktikant@iks-gmbh.com>
2023-03-28 14:16:31 +0200

    General Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {FirstComponent} from "./first/first.component";
+import {SecondComponent} from "./second/second.component";
+import {ThirdComponent} from "./third/third.component";
+import {FourthComponent} from "./fourth/fourth.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "first", component: FirstComponent},
+  {path: "second", component: SecondComponent},
+  {path: "third", component: ThirdComponent},
+  {path: "fourth", component: FourthComponent},
+  {
+    path: "**",
+    component: FirstComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { ThirdComponent } from './third/third.component';
+import { FourthComponent } from './fourth/fourth.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    SecondComponent,
+    ThirdComponent,
+    FourthComponent
   ],
   imports: [
     BrowserModule,
