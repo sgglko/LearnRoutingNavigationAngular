58c883b6bb45f79f3a14193b40758888ce55e559
Praktikant <Praktikant@iks-gmbh.com>
2023-03-28 14:52:34 +0200

    added Lazy Loading & blockit guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,20 +4,32 @@ import {FirstComponent} from "./first/first.component";
 import {SecondComponent} from "./second/second.component";
 import {ThirdComponent} from "./third/third.component";
 import {FourthComponent} from "./fourth/fourth.component";
+import {BlockitGuard} from "./blockit.guard";
 
 const routes: Routes = [
+  //normal Paths
   {path: "first", component: FirstComponent},
   {path: "second", component: SecondComponent},
   {path: "third", component: ThirdComponent},
   {path: "fourth", component: FourthComponent},
-  {
-    path: "**",
-    component: FirstComponent
-  }
+  //no path
+  {path:"", redirectTo: "/first", pathMatch: "full"},
+  //load children lazy loading
+  { path: 'customer',
+    //guard --> blockit.guard.ts to block connection
+    canActivate: [BlockitGuard],
+    loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule) },
+  //wild path
+  {path: "**",component: FirstComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(
+    routes,
+    {
+      enableTracing: true,
+    }
+  )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
